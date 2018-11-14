b87d55cbeea273a31bf330c0b906fd8f826556a6
foresthayes <37276224+foresthayes@users.noreply.github.com>
2018-11-14 10:41:19 -0700

    Added ability to pass additional argument

--- R/utl_jags_trc.R
+++ R/utl_jags_trc.R
@@ -2,24 +2,32 @@
 #'
 #' @param x An object of class rjags (from R2jags) or jagsUI
 #'
-#' @return returns trace plots of tracked parameters without deviance
-#' @export
+#' @return Returns trace plots of tracked parameters
 #'
 #' @examples jags_result %>% util_jags_trc()
 #'
-utl_jags_trc <- function(x){
+#' # plot only density
+#' jags_result %>% utl_jags_trc(type = "density")
+#'
+#'# produce a PDF
+#'jags_result %>% utl_jags_trc(pdf = T)
+#'
+#'# Exclude b0.p from the plots
+#'jags_result %>% utl_jags_trc(excl = "b0.p")
+#'
+#' @export
+utl_jags_trc <- function(x, ..., excl = "deviance", ind = T, pdf = F ){
 
-  # assertthat::assert_that(class(x) == "rjags" | "jagsUI")
+if(class(x) == "rjags"){params <- x$parameters.to.save}
+if(class(x) == "jagsUI"){params <- x$parameters}
 
-  if(class(x) == "rjags"){params <- x$parameters.to.save}
-  if(class(x) == "jagsUI"){params <- x$parameters}
-
-  MCMCvis::MCMCtrace(
-    x,
-    params = params[1:(length(params)-1)],
-    # type = "density",
-    ind = T,
-    pdf = F
-  )
-  return(invisible(x))
+MCMCvis::MCMCtrace(
+  x,
+  ...,
+  params = params,
+  excl = excl,
+  pdf = pdf
+)
+return(invisible(x))
 }
+
